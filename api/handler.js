baecc109900f15ee0410ebc1cff0bffd54f4d83b
LuchiArq <lucianoarquiel@gmail.com>
2020-12-30 03:57:36 -0300

    first commit

---
+++ api/handler.js
@@ -0,0 +1,18 @@
+'use strict';
+const AWS = require('ews-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
